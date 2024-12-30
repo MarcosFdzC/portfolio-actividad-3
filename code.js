720e3a20dc38b923afadc9bde92300113ea53c2d
Marcos Fernandez <marcosfernandezc1998@gmail.com>
2024-12-30 20:47:48 -0300

    js agregado, ah re que solo puse un alert jaja

---
+++ code.js
@@ -0,0 +1,8 @@
+function guardarDatos(){
+    let nombre = document.getElementById("txtNombre").value;
+    let mail = document.getElementById("txtMail").value;
+    let numero = document.getElementById("txtTelefono").value;
+    let mensaje = document.getElementById("txtMensaje").value;
+    alert("Mensaje enviado" + nombre);
+
+}
